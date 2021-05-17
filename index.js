11447bce73a2ccbc149b5c6b7e3f6fa27f234c3f
Ocelots-etc <catherinedgomez@gmail.com>
2021-05-17 17:26:16 -0500

    dry up code with render

--- index.js
+++ index.js
@@ -12,11 +12,14 @@ function getDreams() {
     .then((response) => response.json())
     .then((dreams) => {
       dreams.data.forEach((dream) => {
+        // check that your data is nested in the console so you can 
+        // successfully access the attributes of each individual object
+        render(dream)
       });
     });
 }
 
-function render(dream) {
+function render(dream){
   const dreamMarkup = `
   <div data-id=${dream.id}>
     <img src=${dream.attributes.image_url} width="250">
@@ -29,7 +32,7 @@ function render(dream) {
   document.querySelector("#dream-container").innerHTML += dreamMarkup;
 }
 
-
+  
 function createFormHandler(e) {
   e.preventDefault();
   const dream_datetimeInput = document.querySelector("#input-dream_datetime")
@@ -55,16 +58,7 @@ function postFetch(dream_datetime, journal, image_url, category_id) {
   .then(dream => {
     const dreamData = dream.data
     // render JSON response
-    const dreamMarkup = `
-    <div data-id=${dream.id}>
-      <img src=${dreamData.attributes.image_url} height="200" width="250">
-      <h3>${dreamData.attributes.journal}</h3>
-      <p>${dreamData.attributes.category.name}</p>
-      <button data-id=${dream.id}>edit</button>
-    </div>
-    <br><br>`;
-    
-    document.querySelector('#dream-container').innerHTML += dreamMarkup
+    render(dreamData)
   })
 }
 
